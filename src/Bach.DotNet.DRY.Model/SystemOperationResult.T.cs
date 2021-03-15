76aedec7dff4d37e1da14ccb0d47ca376c059de7
Piotr Bach <piotr.bach.it@gmail.com>
2021-03-15 18:35:58 +0000

    Init system operation result

---
+++ src/Bach.DotNet.DRY.Model/SystemOperationResult.T.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bach.DotNet.DRY.Model
+{
+    public class SystemOperationResult<T> : SystemOperationResult
+    {
+        private SystemOperationResult(bool isOk, T value = default(T), string message = null, Exception exception = null)
+            : base(isOk, message, exception)
+        {
+            this.Value = value;
+        }
+
+        public T Value { get; private set; }
+
+        public static SystemOperationResult<T> Ok(T value = default(T), string message = null)
+        {
+            return new SystemOperationResult<T>(true, value, message);
+        }
+
+        public static SystemOperationResult<T> Warning(T value = default(T), string message = null)
+        {
+            return new SystemOperationResult<T>(false, value, message);
+        }
+
+        public static SystemOperationResult<T> Error(Exception exception, T value = default(T), string message = null)
+        {
+            return new SystemOperationResult<T>(false, value, message, exception);
+        }
+        public static SystemOperationResult<T> Error(T value = default(T), string message = null)
+        {
+            return new SystemOperationResult<T>(false, value, message, null);
+        }
+    }
+}
---
+++ src/Bach.DotNet.DRY.Model/SystemOperationResult.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bach.DotNet.DRY.Model
+{
+    public class SystemOperationResult
+    {
+        protected SystemOperationResult(bool isOk, string message = "", Exception exception = null)
+        {
+            this.IsOk = isOk;
+            this.Message = message;
+            this.Exception = exception;
+        }
+
+        public bool IsOk { get; private set; }
+
+        public string Message { get; private set; }
+
+        public Exception Exception { get; private set; }
+
+        public static SystemOperationResult Ok(string message = "")
+        {
+            return new SystemOperationResult(true, message);
+        }
+
+        public static SystemOperationResult Warning(string message = "")
+        {
+            return new SystemOperationResult(false, message);
+        }
+
+        public static SystemOperationResult Error(string message = "")
+        {
+            return new SystemOperationResult(false, message);
+        }
+
+        public static SystemOperationResult Error(Exception exception, string message = "")
+        {
+            return new SystemOperationResult(false, message, exception);
+        }
+
+        public static implicit operator bool(SystemOperationResult value)
+        {
+            return value.IsOk;
+        }
+
+        public bool HasException => this.Exception != null;
+    }
+}
